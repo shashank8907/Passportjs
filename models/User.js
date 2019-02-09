3e84ed8e13100fb6d2fe72bec3ffc25ecafadf6a
shashank8907 <shashank8907@hotmail.com>
2019-02-10 01:20:09 +0530

    We create a model for the user

---
+++ models/User.js
@@ -0,0 +1,33 @@
+//We create a schema which basically consists of all different fields the we wan4t for a user 
+const mongoose = require(mongoose);
+
+const UserSchema = new mongoose.Schema({
+
+    name:{
+        type: String,
+        required:true
+    },
+
+    email:{
+        type: String,
+        required:true
+    },
+
+    password:{
+        type: String,
+        required:true
+    },
+
+    date:{
+        type: Date,
+        default:Date.now
+    }
+
+
+})
+
+//put the UserSchema in a variable and export it 
+                //To create a mode from our schema model name, Schema
+const User = mongoose.model('User',UserSchema); 
+
+module.exports = User;//Export this object so we can use it in other files
