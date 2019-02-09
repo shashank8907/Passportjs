51374d37fc4dba6997a8bd936e2a8b889ecf5793
shashank8907 <shashank8907@hotmail.com>
2019-02-10 01:38:43 +0530

    Validations of retister

--- routes/users.js
+++ routes/users.js
@@ -18,9 +18,33 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', (req, res) => {
+    const {name, email, password, password2} = req.body;
+    //Validation before we submit We can also use third party libraries for this
+    let errors = [];
+    
+    //Check required fields
+    if(!name || !email || !password || !password2){
+        errors.push({message:"Please fill in all fields"});
+    }
+    //Check passwords match
+    if(password !== password2){
+        errors.push({message:"Passwords fdo not match"});
+    }
+    //Check passwords length
+    if(password.length < 6){
+        errors.push({message:"Password should be atleast 6 characters"});
+    }
 
-    console.log(req.body);
-    res.send("Hey");
+    if(errors.length > 0){
+        //We have an issue 
+        // With ejs or any templet engin you can pass in values along with rendering page
+        res.render('register',{
+         errors,
+            
+        }); 
+    }else{
+        res.send('pass');
+    }
 });
 
 
